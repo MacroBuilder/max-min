457ddfc26a8a8f49e20c7cb34ee0e4d64449547f
T.C. <thomas.n.cool@gmail.com>
2015-06-24 16:38:22 -0700

    Update max-min.py

--- max-min.py
+++ max-min.py
@@ -1,6 +1,6 @@
 def max_min(array, n, k):
     array = sorted(array)
-    return min(array[a+k-1]-array[a] for a in range(n - k + 1))
+    return min(array[a + k - 1] - array[a] for a in range(n - k + 1))
 
 N = int(raw_input().strip())
 K = int(raw_input().strip())
